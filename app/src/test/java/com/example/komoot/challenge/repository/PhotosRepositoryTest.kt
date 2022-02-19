9a91e13e98fc1cd8be09cad05daefe53330b6cad
Vladimir Sechkarev <sechkarevs@gmail.com>
2022-02-19 20:47:22 +0100

    add more tests

--- app/src/test/java/com/example/komoot/challenge/repository/PhotosRepositoryTest.kt
+++ app/src/test/java/com/example/komoot/challenge/repository/PhotosRepositoryTest.kt
@@ -19,14 +19,6 @@ class PhotosRepositoryTest {
     @Before
     fun setUp() {
         repository = PhotosRepository(flickr)
-    }
-
-    @Test
-    fun `flickr interface returns a photo, url matches photo parameters`() {
-        val location = mockk<Location>() {
-            every { latitude } returns 10.0
-            every { longitude } returns 10.0
-        }
         val photo = Photo().apply {
             server = "12345"
             id = "976543"
@@ -37,9 +29,48 @@ class PhotosRepositoryTest {
         } returns PhotoList<Photo>().apply {
             add(photo)
         }
+    }
+
+    @Test
+    fun `flickr interface returns a photo, url matches photo parameters`() {
+        val location = mockk<Location> {
+            every { latitude } returns 10.0
+            every { longitude } returns 10.0
+        }
 
         val url = repository.retrievePhotoUrl(location)
 
         assertEquals("https://live.staticflickr.com/12345/976543_abcdefg.jpg", url)
+    }
+
+    @Test
+    fun `method twice called from the same location, second call returns null`() {
+        val location = mockk<Location> {
+            every { latitude } returns 10.0
+            every { longitude } returns 10.0
+            every { distanceTo(any()) } returns 0f
+        }
+
+        val url1 = repository.retrievePhotoUrl(location)
+        val url2 = repository.retrievePhotoUrl(location)
+
+        assertNotNull(url1)
+        assertNull(url2)
+    }
+
+    @Test
+    fun `method twice called from distinct locations which are far away, second url is not null`() {
+        val location = mockk<Location> {
+            every { latitude } returns 10.0
+            every { longitude } returns 10.0
+            every { distanceTo(any()) } returns 1000f
+        }
+
+        val url1 = repository.retrievePhotoUrl(location)
+        val url2 = repository.retrievePhotoUrl(location)
+
+        assertNotNull(url1)
+        assertNotNull(url2)
+        assertEquals(url1, url2)
     }
 }
