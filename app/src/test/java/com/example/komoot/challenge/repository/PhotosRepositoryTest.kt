8630df361ef987ac5845ee53d360fac2f08c4312
Vladimir Sechkarev <sechkarevs@gmail.com>
2022-02-20 11:36:45 +0100

    add one more test

--- app/src/test/java/com/example/komoot/challenge/repository/PhotosRepositoryTest.kt
+++ app/src/test/java/com/example/komoot/challenge/repository/PhotosRepositoryTest.kt
@@ -10,6 +10,7 @@ import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Before
 import org.junit.Test
 
@@ -72,7 +73,7 @@ class PhotosRepositoryTest {
     }
 
     @Test
-    fun `method called twice from distinct locations which are far from each other, flow is triggered twice`() {
+    fun `method called twice from distinct locations which are far from each other, the same photo is returned, flow is triggered twice with the same photo`() {
         val location = mockk<Location> {
             every { latitude } returns 10.0
             every { longitude } returns 10.0
@@ -88,6 +89,45 @@ class PhotosRepositoryTest {
                 assertEquals(1, urlList1.size)
                 assertEquals(1, urlList2.size)
                 assertEquals(urlList1[0], urlList2[0])
+            }
+        }
+    }
+
+    @Test
+    fun `method called twice from distinct locations which are far from each other, different photos are returned, flow is triggered twice with first 1 and then 2 photos`() {
+        val photo1 = Photo().apply {
+            server = "12345"
+            id = "976543"
+            secret = "abcdefg"
+        }
+        val photo2 = Photo().apply {
+            server = "67890"
+            id = "324654"
+            secret = "zxcvbn"
+        }
+        every {
+            flickr.photosInterface.search(any(), any(), any())
+        } returns PhotoList<Photo>().apply {
+            add(photo1)
+        } andThen PhotoList<Photo>().apply {
+            add(photo2)
+        }
+        val location = mockk<Location> {
+            every { latitude } returns 10.0
+            every { longitude } returns 10.0
+            every { distanceTo(any()) } returns 1000f
+        }
+
+        runTest {
+            repository.photoUrlsFlow.test {
+                repository.retrieveNewPhoto(location)
+                val urlList1 = awaitItem()
+                repository.retrieveNewPhoto(location)
+                val urlList2 = awaitItem()
+                assertEquals(1, urlList1.size)
+                assertEquals(2, urlList2.size)
+                assertNotEquals(urlList1[0], urlList2[0])
+                assertEquals(urlList1[0], urlList2[1])
             }
         }
     }
